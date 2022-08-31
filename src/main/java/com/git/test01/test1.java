2c928c3fa96805c01d6cb32c474d653a044715ba
Delia <1726764956@qq.com>
2022-08-31 14:26:31 +0800

    hot-fix commit01

--- src/main/java/com/git/test01/test1.java
+++ src/main/java/com/git/test01/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("我是idea 我已经添加git连接了 哈哈哈");
         System.out.println(2+3+"5555");
+        System.out.println("hello i am hot-fix!");
     }
 }
