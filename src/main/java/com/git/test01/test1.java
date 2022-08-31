6771530e845b19e843d13a07c91f225d938b56ef
Delia <1726764956@qq.com>
2022-08-31 14:18:38 +0800

    second commit!

--- src/main/java/com/git/test01/test1.java
+++ src/main/java/com/git/test01/test1.java
@@ -3,5 +3,6 @@ package com.git.test01;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("我是idea 我已经添加git连接了 哈哈哈");
+        System.out.println(2+3+"5555");
     }
 }
